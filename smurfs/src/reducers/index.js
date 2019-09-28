fb5fc6f55f709dac8f8c7d2d81a05d21bfc23bd5
Carlos Mitchell <kakismitchell@gmail.com>
2019-09-27 21:38:17 -0400

    actions and reducers Done

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,36 @@
+import {
+    FETCH_SMURFS_START,
+    FETCH_SMURFS_SUCCESS,
+    FETCH_SMURFS_FAILURE
+  } from "../actions";
+  
+  const initialState = {
+    smurfs: [],
+    error: ""
+  };
+  
+  export const reducer = (state = initialState, action) => {
+    switch (action.type) { 
+
+      case FETCH_SMURFS_START:
+        return {
+          ...state,
+          error: ""
+        };
+      case FETCH_SMURFS_SUCCESS:
+
+        return {
+          ...state,
+          smurfs: action.payload,
+          error: ""
+        };
+      case FETCH_SMURFS_FAILURE:
+          
+        return {
+          ...state,
+          error: action.payload
+        };
+      default:
+        return state;
+    }
+  };
